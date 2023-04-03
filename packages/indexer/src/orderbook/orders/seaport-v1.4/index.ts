b2cac856338d20c4f76beefe1bb9e6473eb0560f
Joey <joey.xf@gmail.com>
2023-04-03 11:10:30 +0800

    feat: put common seaport orderbook code to base

--- packages/indexer/src/orderbook/orders/seaport-v1.4/index.ts
+++ packages/indexer/src/orderbook/orders/seaport-v1.4/index.ts
@@ -1285,7 +1285,7 @@ export const save = async (
     );
 
     if (relayToArweave) {
-      await arweaveRelay.addPendingOrdersSeaportV14(arweaveData);
+      await arweaveRelay.addPendingOrdersSeaport(arweaveData);
     }
   }
 
