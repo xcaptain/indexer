fd3734149aaa82b2f5dc372710d332e1e3cc25fe
Joey <joey.xf@gmail.com>
2023-04-10 19:12:00 +0800

    feat: add goerli address

--- packages/sdk/src/alienswap/addresses.ts
+++ packages/sdk/src/alienswap/addresses.ts
@@ -2,5 +2,5 @@ import { ChainIdToAddress, Network } from "../utils";
 
 export const Exchange: ChainIdToAddress = {
   [Network.Ethereum]: "",
-  [Network.EthereumGoerli]: "",
+  [Network.EthereumGoerli]: "0x05955CDdd8B737dAC33a03E70B3e15B6C4b0D765",
 };
